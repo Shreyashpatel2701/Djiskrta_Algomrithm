b69ac651e7c0a638d0aa81a6e9c5ea7b4f3351c9
Shreyash Patel <65528297+Shreyashpatel2701@users.noreply.github.com>
2021-04-23 13:42:10 +0530

    Add files via upload

---
+++ Djiskrta Algomrithm/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import gui.MainWindow;
+
+import javax.swing.*;
+import java.awt.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {  }
+
+        JFrame j = new JFrame();
+        j.setTitle("Dijkstra Algorithm");
+
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setSize(new Dimension(900, 600));
+        j.add(new MainWindow());
+        j.setVisible(true);
+
+    }
+    }
+
